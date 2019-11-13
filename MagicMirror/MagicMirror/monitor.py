64a6dab45d426dbed579c5653c0b728d4eeb5276
zeng.ande <zengande@outlook.com>
2019-11-13 11:18:44 +0800

    修改为 namespace 模式

---
+++ MagicMirror/MagicMirror/monitor.py
@@ -0,0 +1,36 @@
+from flask_socketio import Namespace, emit
+from threading import Lock
+from MagicMirror import socketio
+#import Adafruit_DHT
+
+thread = None
+thread_lock = Lock()
+connections = 0
+
+#sensor = Adafruit_DHT.DHT11
+#pin = 4
+
+class Monitor(Namespace):
+    """description of class"""
+    def on_connect(self):
+        global thread, thread_lock, connections
+        with thread_lock:
+            connections += 1
+            if thread is None:
+                thread = socketio.start_background_task(target=self._read_ht_thread)
+
+    def on_disconnect(self):
+        global thread, thread_lock, connections
+        with thread_lock:
+            connections -= 1
+            if connections == 0:
+                thread.join()
+
+    def _read_ht_thread(self):
+        global connections
+        while connections > 0:
+            humidity, temperature =(None,None) # Adafruit_DHT.read_retry(sensor, pin)
+            socketio.emit('response_ht',
+                          {'humidity': humidity, 'temperature': temperature},
+                          namespace='/monitor')
+            socketio.sleep(2)
--- MagicMirror/MagicMirror/views.py
+++ MagicMirror/MagicMirror/views.py
@@ -1,20 +1,10 @@
 """
 Routes and views for the flask application.
 """
-
-import psutil
 import time
-from threading import Lock
 from flask import Flask
-from flask_socketio import SocketIO
 from MagicMirror import app,socketio
-#import Adafruit_DHT
-
-thread = None
-thread_lock = Lock()
-
-#sensor = Adafruit_DHT.DHT11
-#pin = 4
+from MagicMirror.monitor import Monitor
 
 @app.route('/')
 @app.route('/home')
@@ -22,18 +12,4 @@ def home():
     """Renders the home page."""
     return app.send_static_file('html/index.html')
 
-@socketio.on('connect', namespace="/monitor")
-def connect():
-    print('connected!')
-    global thread
-    with thread_lock:
-        if thread is None:
-            thread = socketio.start_background_task(target=read_ht_thread)
-
-def read_ht_thread():
-    while True:
-        humidity, temperature =(0,0) # Adafruit_DHT.read_retry(sensor, pin)
-        socketio.emit('response_ht',
-                      {'humidity': humidity, 'temperature': temperature},
-                      namespace='/monitor')
-        socketio.sleep(2)
+socketio.on_namespace(Monitor("/monitor"))
